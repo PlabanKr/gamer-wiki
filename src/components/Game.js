99eb13f3dbf5bc8c20820494c0ac40921b9c9d46
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-05-29 00:16:18 +0530

    add tooltip

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,7 @@ import '../styles/Game.css'
 import { searchByCategory } from '../api/api'
 import { useState } from 'react'
 import { BsSearch } from 'react-icons/bs'
+import { AiOutlineInfoCircle } from 'react-icons/ai'
 import GameCard from './GameCard'
 
 const Game = () => {
@@ -14,6 +15,7 @@ const Game = () => {
 
     const handleSearch = async (e) => {
         e.preventDefault()
+        setSearchResult(null)
         let res = await searchByCategory(categoryName)
         setSearchResult(res)
     }
@@ -21,6 +23,15 @@ const Game = () => {
     return (
     <div className='mainGameBody'>
         <form>
+            <span
+            className='info'
+            data-tool-tip='All categories:
+            mmorpg, shooter, strategy, moba, racing, sports, social, sandbox, open-world, survival, pvp, pve, pixel, voxel, 
+            zombie, turn-based, first-person, third-Person, top-down, tank, space, sailing, side-scroller, superhero, permadeath, card, battle-royale, 
+            mmo, mmofps, mmotps, 3d, 2d, anime, fantasy, sci-fi, fighting, action-rpg, action, military, martial-arts, flight, low-spec, tower-defense, horror, mmorts'
+            >
+                <AiOutlineInfoCircle/>
+            </span>
             <input type='text' className='searchInput' placeholder='Enter category (eg. shooter, strategy)' onChange={handleGameName} />
             <button className='search' onClick={handleSearch}><BsSearch /></button>
         </form>
