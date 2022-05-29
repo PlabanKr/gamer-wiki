98aaa2e8a379cba1720c02618bf806da8241d725
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-05-29 13:55:34 +0530

    change tool tip position

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,52 +1,60 @@
-import '../styles/Game.css'
-import { searchByCategory } from '../api/api'
-import { useState } from 'react'
-import { BsSearch } from 'react-icons/bs'
-import { AiOutlineInfoCircle } from 'react-icons/ai'
-import GameCard from './GameCard'
+import "../styles/Game.css";
+import { searchByCategory } from "../api/api";
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import GameCard from "./GameCard";
 
 const Game = () => {
-    const [categoryName, setCategoryName] = useState('')
-    const [searchResult, setSearchResult] = useState(null)
+  const [categoryName, setCategoryName] = useState("");
+  const [searchResult, setSearchResult] = useState(null);
 
-    const handleGameName = (e) => {
-        setCategoryName(e.target.value)
-    }
+  const handleGameName = (e) => {
+    setCategoryName(e.target.value);
+  };
 
-    const handleSearch = async (e) => {
-        e.preventDefault()
-        setSearchResult(null)
-        let res = await searchByCategory(categoryName)
-        setSearchResult(res)
-    }
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    setSearchResult(null);
+    let res = await searchByCategory(categoryName);
+    setSearchResult(res);
+  };
 
-    return (
-    <div className='mainGameBody'>
-        <form>
-            <span
-            className='info'
-            data-tool-tip='All categories:
+  return (
+    <div className="mainGameBody">
+      <form>
+        <input
+          type="text"
+          className="searchInput"
+          placeholder="Enter category (eg. shooter, strategy)"
+          onChange={handleGameName}
+        />
+        <button className="search" onClick={handleSearch}>
+          <BsSearch />
+        </button>
+      </form>
+      <div className="result">
+        {searchResult == null ? (
+          <p>No Results</p>
+        ) : (
+          searchResult.map((data) => {
+            return <GameCard data={data} key={data.id} />;
+          })
+        )}
+      </div>
+      <div className="info-container">
+        <span
+          className="info"
+          data-tool-tip="All categories: \n
             mmorpg, shooter, strategy, moba, racing, sports, social, sandbox, open-world, survival, pvp, pve, pixel, voxel, 
             zombie, turn-based, first-person, third-Person, top-down, tank, space, sailing, side-scroller, superhero, permadeath, card, battle-royale, 
-            mmo, mmofps, mmotps, 3d, 2d, anime, fantasy, sci-fi, fighting, action-rpg, action, military, martial-arts, flight, low-spec, tower-defense, horror, mmorts'
-            >
-                <AiOutlineInfoCircle/>
-            </span>
-            <input type='text' className='searchInput' placeholder='Enter category (eg. shooter, strategy)' onChange={handleGameName} />
-            <button className='search' onClick={handleSearch}><BsSearch /></button>
-        </form>
-        <div className='result'>
-            {   searchResult == null  ?
-                    <p>No Results</p> 
-                    :
-                    searchResult.map((data) => {
-                    return <GameCard data={data} key={data.id} />
-                    })
-                
-            }
-        </div>
+            mmo, mmofps, mmotps, 3d, 2d, anime, fantasy, sci-fi, fighting, action-rpg, action, military, martial-arts, flight, low-spec, tower-defense, horror, mmorts"
+        >
+          <AiOutlineInfoCircle />
+        </span>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Game
+export default Game;
